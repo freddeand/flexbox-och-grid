26256f1d7165697000658ff879068ca2afbc38d1
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-09-19 13:45:36 +0200

    fixed

--- meny/meny2/meny.js
+++
@@ -1,8 +0,0 @@
-<script>
-  const menuIcon = document.querySelector('.nav-icon');
-  const menu = document.querySelector('.nav');
-
-  menuIcon.addEventListener('click', () => {
-    menu.classList.toggle('active'); // Lägg till eller ta bort klassen "active" för att visa/gömma menyn
-  });
-</script>
